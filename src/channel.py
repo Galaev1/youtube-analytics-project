762bfc85d5e0583184a1c0bc827084467549dee6
Galaev1 <Amin29_07@mail.ru>
2023-04-05 00:12:02 +0300

    первые изменения

--- src/channel.py
+++ src/channel.py
@@ -1,12 +1,11 @@
 
-
 class Channel:
     """Класс для ютуб-канала"""
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+        self.channel_id = channel_id
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        pass
+        return self.channel_id
