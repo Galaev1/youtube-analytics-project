cb5fe1de503b122788b42a92b8b8e9f2033a430f
Galaev1 <Amin29_07@mail.ru>
2023-04-07 22:40:08 +0300

    channel.py изменения

--- src/channel.py
+++ src/channel.py
@@ -4,20 +4,28 @@ from distutils.command.build import build
 
 from helper.youtube_api_manual import youtube
 
+
+
+
 class Channel:
     """Класс для ютуб-канала"""
     api_key: str = os.getenv('YT_API_KEY')
     service = []
 
-    def __init__(self, channel_id: str, title: str, description: str,url,subscriberCount: int,videoCount: int,viewCount: int) -> None:
+    def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
         self.channel_id = channel_id
-        self.title = self.channel_id['items'][0]['snippet']['title']
-        self.description = self.channel_id['items'][0]['snippet']['description']
-        self.url = self.channel_id['items'][0]['thumbnails']['url']
-        self.subscriberCount = self.channel_id['items'][0]['statistics']['subscriberCount']
-        self.videoCount = self.channel_id['items'][0]['statistics']['videoCount']
-        self.viewCount = self.channel_id['items'][0]['statistics']['viewCount']
+        video_response = youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
+                                               id=channel_id
+                                               ).execute()
+
+
+        self.title: int = video_response['items'][0]['snippet']['title']
+        self.description: str = video_response['items'][0]['snippet']['description']
+        self.url: str = video_response['items'][0]['thumbnails']['url']
+        self.subscriberCount: str = video_response['items'][0]['statistics']['subscriberCount']
+        self.videoCount: str = video_response['items'][0]['statistics']['videoCount']
+        self.viewCount: str = video_response['items'][0]['statistics']['viewCount']
         self.youtube = build('youtube', 'v3', developerKey=self.api_key)
         Channel.service.append(self)
 
