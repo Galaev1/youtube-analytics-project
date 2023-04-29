10e330d122859f069c9c3ae4b8c962fe360f8f26
Galaev1 <Amin29_07@mail.ru>
2023-04-29 23:21:08 +0300

    доработка video.py

--- src/video.py
+++ src/video.py
@@ -1,4 +1,3 @@
-import json
 import os
 
 from googleapiclient.discovery import build
@@ -9,14 +8,23 @@ class Video:
     api_key: str = os.getenv('YT_API_KEY')
     youtube = build('youtube', 'v3', developerKey=api_key)
     def __init__(self, video_id: str) -> None:
-        self.__video_id = video_id
-        self.video_response = self.youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
+        try:
+            self.__video_id = video_id
+            self.video_response = self.youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
                                                id=self.__video_id
                                                ).execute()
-        self.video_title: str = self.video_response['items'][0]['snippet']['title']
-        self.view_count: int = self.video_response['items'][0]['statistics']['viewCount']
-        self.like_count: int = self.video_response['items'][0]['statistics']['likeCount']
-        self.comment_count: int = self.video_response['items'][0]['statistics']['commentCount']
+            self.video_title: str = self.video_response['items'][0]['snippet']['title']
+            self.view_count: int = self.video_response['items'][0]['statistics']['viewCount']
+            self.like_count: int = self.video_response['items'][0]['statistics']['likeCount']
+            self.comment_count: int = self.video_response['items'][0]['statistics']['commentCount']
+        except IndexError:
+            self.title = None
+            self.like_count = None
+            print("IndexError")
+
+    def __str__(self) -> str:
+        return self.title
+
 
 class PLVideo(Video):
     def __init__(self, video_id: str, playlist_ids: str):
